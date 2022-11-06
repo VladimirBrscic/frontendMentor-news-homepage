1840babf87a3167ac682cd17ee9184043699cc65
Vladimir Brscic <vlado.brscic@gmail.com>
2022-11-06 18:58:09 +0000

    mobile menu

--- main.js
+++ main.js
@@ -1,13 +1,18 @@
-const mobileMenuSwitch = document.getElementById('mobile-menu-switch');
-const hamburgerMenu = document.getElementById('hamburger-menu');
-const closeMenu = document.getElementById('close-menu');
-const menu = document.getElementById('menu');
-
+const mobileMenuSwitch = document.querySelector('.mobile-menu-switch');
+const hamburgerMenu = document.querySelector('.hamburger-menu');
+const closeMenu = document.querySelector('.close-menu');
+const menu = document.querySelector('.nav-menu');
 
 
 
 mobileMenuSwitch.addEventListener('click',()=>{
+    
     closeMenu.classList.toggle('hide');
     hamburgerMenu.classList.toggle('hide');
-    menu.classList.toggle('hide')
-})
+    menu.classList.toggle('hide');
+    
+    let buttonAria = mobileMenuSwitch.getAttribute('aria-expanded');
+
+    buttonAria === 'false' ? buttonAria = 'true' : buttonAria = 'false';
+    mobileMenuSwitch.setAttribute('aria-expanded', buttonAria);
+    })
